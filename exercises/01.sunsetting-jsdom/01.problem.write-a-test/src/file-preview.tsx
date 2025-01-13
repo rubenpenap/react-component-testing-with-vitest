b42ab1de36cb54758aa509181064f82db2fa1b63
Artem Zakharchenko <kettanaito@gmail.com>
2025-01-13 15:46:46 +0100

    make filename font size larger

--- exercises/01.sunsetting-jsdom/01.problem.write-a-test/src/file-preview.tsx
+++ exercises/01.sunsetting-jsdom/01.problem.write-a-test/src/file-preview.tsx
@@ -17,7 +17,7 @@ export function FilePreview({ file }: { file: File }) {
 	return (
 		<div>
 			<div className="w-full max-w-2xl overflow-hidden rounded-md border border-slate-200 bg-white shadow-lg shadow-slate-200">
-				<p className="border-b border-slate-200 bg-slate-50 px-4 py-2 text-sm font-bold text-slate-600">
+				<p className="border-b border-slate-200 bg-slate-50 px-4 py-2 font-bold text-slate-600">
 					📄 {file.name}
 				</p>
 				<pre className="max-h-[28ch] overflow-scroll p-4">{previewText}</pre>
