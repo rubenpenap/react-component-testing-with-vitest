34e0710b5da00483e7892c249f302bae5a486d82
Artem Zakharchenko <kettanaito@gmail.com>
2025-01-14 14:37:51 +0100

    02/03: add playwright solution, merge vitest with vite

--- exercises/02.vitest-browser-mode/03.solution.use-playwright/vite.config.ts
+++ exercises/02.vitest-browser-mode/03.solution.use-playwright/vite.config.ts
@@ -5,6 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	test: {
-		environment: 'jsdom',
+		browser: {
+			enabled: true,
+			provider: 'playwright',
+			instances: [
+				{
+					browser: 'chromium',
+				},
+			],
+		},
 	},
 })
