85d1e6d1d39c69dc6a4c4692ebf6aa1bb0f8cb2b
Artem Zakharchenko <kettanaito@gmail.com>
2025-01-14 14:49:49 +0100

    02/04: add solution, proper ts configs

---
+++ exercises/02.vitest-browser-mode/04.solution.use-dom-assertions/vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+	plugins: [react()],
+	test: {
+		browser: {
+			enabled: true,
+			provider: 'playwright',
+			instances: [
+				{
+					browser: 'chromium',
+				},
+			],
+		},
+	},
+})
