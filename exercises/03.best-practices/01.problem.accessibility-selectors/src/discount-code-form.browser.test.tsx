622662bb6ea3c897504f85f6aca1016b859fe0d3
Artem Zakharchenko <kettanaito@gmail.com>
2025-02-03 16:57:38 +0100

    03/01: add problem and solution text

---
+++ exercises/03.best-practices/01.problem.accessibility-selectors/src/discount-code-form.browser.test.tsx
@@ -0,0 +1,21 @@
+import { page } from '@vitest/browser/context'
+import { render } from 'vitest-browser-react'
+import { DiscountCodeForm } from './discount-code-form.js'
+
+test('renders the discount form', async () => {
+	render(<DiscountCodeForm />)
+
+	// 🐨 Create a variable called `discountInput` and assign it
+	// the result of locating the element by label text "Discount code".
+	// 💰 const discountInput = page.getByLabelText(labelText)
+	//
+	// 🐨 Write an assertion that the `discountInput` element is visible.
+	// 💰 expect.element(locator).toBeVisible()
+	//
+	// 🐨 Create a new variable called `applyDiscountButton` and assign it
+	// the result of locating the element by role 'button' and accessible name
+	// 'Apply discount'.
+	// 💰 const applyDiscountButton = page.getByRole(role, { name: accessibleName })
+	//
+	// 🐨 Finally, write another assertion that the apply discount button is visible.
+})
---
+++ exercises/03.best-practices/01.problem.accessibility-selectors/src/discount-code-form.tsx
@@ -0,0 +1,57 @@
+import { useState, type FormEventHandler } from 'react'
+
+export interface Discount {
+	code: string
+	amount: number
+}
+
+export function DiscountCodeForm() {
+	const [appliedDiscount, setAppliedDiscount] = useState<Discount>()
+
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+		event.preventDefault()
+
+		const data = new FormData(event.currentTarget)
+		const code = data.get('discountCode') as string
+
+		setAppliedDiscount({
+			code,
+			amount: 20,
+		})
+	}
+
+	return (
+		<section className="w-96 rounded-lg border bg-white p-10">
+			{appliedDiscount ? (
+				<p>
+					Discount: <strong>{appliedDiscount.code}</strong> (-
+					{appliedDiscount.amount}%)
+				</p>
+			) : (
+				<form onSubmit={handleSubmit} className="flex flex-col gap-5">
+					<div>
+						<label htmlFor="discountCode" className="mb-1 block">
+							Discount code
+						</label>
+						<input
+							id="discountCode"
+							name="discountCode"
+							className="w-full rounded-md border px-2 py-1 focus:ring-4"
+							placeholder="ABCD1234"
+							pattern="[A-Z]{4}[0-9]{4}"
+							autoComplete="off"
+							required
+						/>
+					</div>
+
+					<button
+						type="submit"
+						className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-500 focus:ring-4"
+					>
+						Apply discount
+					</button>
+				</form>
+			)}
+		</section>
+	)
+}
