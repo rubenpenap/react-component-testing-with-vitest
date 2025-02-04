5d80eb5115b4a4cb7d34f3aa21a90bb8183f7024
Artem Zakharchenko <kettanaito@gmail.com>
2025-02-04 12:27:50 +0100

    03/03: add problem and solution texts

---
+++ exercises/03.best-practices/03.problem.network-mocking/src/discount-code-form.browser.test.tsx
@@ -0,0 +1,86 @@
+import { page } from '@vitest/browser/context'
+import { render } from 'vitest-browser-react'
+import { http, HttpResponse } from 'msw'
+// 🐨 Import the `test` function from `test-extend.js`.
+// This custom `test` function exposes the `worker` object
+// you will use to access and use MSW in tests.
+// 💰 import { test } from '../test-extend.js'
+import { DiscountCodeForm } from './discount-code-form.js'
+
+test('applies a discount code', async () => {
+	render(<DiscountCodeForm />)
+
+	const discountInput = page.getByLabelText('Discount code')
+	await discountInput.fill('EPIC2025')
+
+	const applyDiscountButton = page.getByRole('button', {
+		name: 'Apply discount',
+	})
+	await applyDiscountButton.click()
+
+	await expect
+		.element(page.getByText('Discount: EPIC2025 (-20%)'))
+		.toBeVisible()
+})
+
+test('displays a warning for legacy discount codes', async ({
+	// 🐨 Access the custom `worker` fixture you've prepared earlier.
+}) => {
+	// 🐨 Call `worker.use()` and provide it a new request handler
+	// for the same POST https://api.example.com/discount/code request.
+	// In this handler, respond with a different mocked response that
+	// returns `isLegacy: true` in its JSON payload.
+	//
+	// Use the existing happy-path request handler from `src/mocks/handlers.ts`
+	// as a reference!
+	//
+	// 💰 worker.use()
+	// 💰 http.post(predicate, resolver)
+	// 💰 HttpResponse.json({ code, amount, isLegacy })
+
+	render(<DiscountCodeForm />)
+
+	const discountInput = page.getByLabelText('Discount code')
+	await discountInput.fill('LEGA2000')
+
+	const applyDiscountButton = page.getByRole('button', {
+		name: 'Apply discount',
+	})
+	await applyDiscountButton.click()
+
+	// 🐨 Write an assertion that expects the text element
+	// with the applied discount code to be visible on the page.
+	//
+	// 🐨 Write another assertion that expected the warning
+	// to appear, notifying the user about the legacy discount code.
+	// 💰 await expect.element(locator).toHaveTextContent(content)
+	// 💰 page.getByRole('alert')
+	// 💰 '"LEGA2000" is a legacy code. Discount amount halfed.'
+})
+
+test('displays an error when fetching the discount fails', async ({
+	// 🐨 Access the `worker` fixture here.
+}) => {
+	// 🐨 Call `worker.use()` and describe another request handler.
+	// This time, respond with a mocked 500 response to simulate
+	// server error.
+	// 💰 worker.use(handler)
+	// 💰 http.post(predicate, resolver)
+	// 💰 new HttpResponse(null, { status: 500 })
+
+	render(<DiscountCodeForm />)
+
+	const discountInput = page.getByLabelText('Discount code')
+	await discountInput.fill('CODE1234')
+
+	const applyDiscountButton = page.getByRole('button', {
+		name: 'Apply discount',
+	})
+	await applyDiscountButton.click()
+
+	// 🐨 Write an assertion that a notification is displayed,
+	// saying that applying the discount code has failed.
+	// 💰 await expect.element(locator).toHaveTextContent(content)
+	// 💰 page.getByRole('alert')
+	// 💰 'Failed to apply the discount code'
+})
---
+++ exercises/03.best-practices/03.problem.network-mocking/src/discount-code-form.tsx
@@ -0,0 +1,153 @@
+import { useReducer, useState, type FormEventHandler } from 'react'
+
+interface Notification {
+	type: 'error' | 'warning'
+	text: string
+}
+
+export interface Discount {
+	code: string
+	amount: number
+	isLegacy?: true
+}
+
+type DiscountFormState =
+	| { submitting: false; discount?: never }
+	| { submitting: false; discount: Discount }
+	| { submitting: true }
+
+type DiscountFormAction =
+	| { type: 'idle' }
+	| { type: 'submitting' }
+	| { type: 'success'; discount: Discount }
+
+function discountFormReducer(
+	_state: DiscountFormState,
+	action: DiscountFormAction,
+): DiscountFormState {
+	switch (action.type) {
+		case 'idle': {
+			return {
+				submitting: false,
+			}
+		}
+		case 'submitting': {
+			return {
+				submitting: true,
+			}
+		}
+		case 'success': {
+			return {
+				submitting: false,
+				discount: action.discount,
+			}
+		}
+	}
+}
+
+async function fetchDiscount(code: string): Promise<Discount> {
+	const response = await fetch('https://api.example.com/discount/code', {
+		method: 'POST',
+		body: code,
+	})
+	const data = (await response.json()) as Discount
+	return data
+}
+
+export function DiscountCodeForm() {
+	const [state, dispatch] = useReducer(discountFormReducer, {
+		submitting: false,
+	})
+	const [notification, setNotification] = useState<Notification>()
+
+	const notify = (text: string, type: Notification['type'] = 'warning') => {
+		setNotification({ type, text })
+		setTimeout(() => setNotification(undefined), 5000)
+	}
+
+	const handleApplyDiscount: FormEventHandler<HTMLFormElement> = async (
+		event,
+	) => {
+		event.preventDefault()
+		setNotification(undefined)
+		dispatch({ type: 'submitting' })
+
+		const data = new FormData(event.currentTarget)
+		const code = data.get('discountCode')
+
+		if (!code) {
+			notify('Missing discount code', 'error')
+			return
+		}
+
+		if (typeof code !== 'string') {
+			notify(
+				`Expected discount code to be a string but got ${typeof code}`,
+				'error',
+			)
+			return
+		}
+
+		fetchDiscount(code)
+			.then((discount) => {
+				dispatch({ type: 'success', discount })
+
+				if (discount.isLegacy) {
+					notify(`"${code}" is a legacy code. Discount amount halfed.`)
+				}
+			})
+			.catch((error) => {
+				console.error(error)
+				notify('Failed to apply the discount code', 'error')
+				dispatch({ type: 'idle' })
+			})
+	}
+
+	return (
+		<section className="w-96 rounded-lg border bg-white p-10">
+			{state.submitting ? (
+				<p aria-hidden>
+					<span className="inline-block animate-spin font-bold">{'◡'}</span>
+				</p>
+			) : state.discount ? (
+				<p>
+					Discount: <strong>{state.discount.code}</strong> (-
+					{state.discount.amount}%)
+				</p>
+			) : (
+				<form onSubmit={handleApplyDiscount} className="flex flex-col gap-5">
+					<div>
+						<label htmlFor="discountCode" className="mb-1 block">
+							Discount code
+						</label>
+						<input
+							id="discountCode"
+							name="discountCode"
+							className="w-full rounded-md border px-2 py-1 focus:ring-4"
+							placeholder="ABCD1234"
+							pattern="[A-Z]{4}[0-9]{4}"
+							autoComplete="off"
+							required
+						/>
+					</div>
+
+					<button
+						type="submit"
+						className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-500 focus:ring-4"
+					>
+						Apply discount
+					</button>
+				</form>
+			)}
+
+			{notification ? (
+				<p
+					role="alert"
+					className={`animation-slide animate-slide-in fixed bottom-5 right-5 rounded-lg border px-5 py-2.5 font-medium ${notification.type === 'error' ? 'border-red-800/20 bg-red-200' : 'border-yellow-800/20 bg-yellow-200'}`}
+				>
+					{notification.text}
+				</p>
+			) : null}
+		</section>
+	)
+}
