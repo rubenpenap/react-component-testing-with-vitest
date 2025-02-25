50efb42fafe43b259e21f4052837721bb093a276
Artem Zakharchenko <kettanaito@gmail.com>
2025-02-25 11:44:30 +0100

    format all the files

--- exercises/04.debugging/01.problem.dom-snapshots/src/tic-tac-toe.tsx
+++ exercises/04.debugging/01.problem.dom-snapshots/src/tic-tac-toe.tsx
@@ -3,15 +3,15 @@ import { useState } from 'react'
 export function TicTacToe() {
 	return (
 		<div className="grid grid-cols-3 grid-rows-3">
-			<Button aria-label="top left" className="border-l-0 border-t-0" />
+			<Button aria-label="top left" className="border-t-0 border-l-0" />
 			<Button aria-label="top middle" className="border-t-0" />
-			<Button aria-label="top right" className="border-r-0 border-t-0" />
+			<Button aria-label="top right" className="border-t-0 border-r-0" />
 			<Button aria-label="left middle" className="border-l-0" />
 			<Button aria-label="middle" />
 			<Button aria-label="bottom left" className="border-r-0" />
 			<Button aria-label="right middle" className="border-b-0 border-l-0" />
 			<Button aria-label="bottom middle" className="border-b-0" />
-			<Button aria-label="bottom right" className="border-b-0 border-r-0" />
+			<Button aria-label="bottom right" className="border-r-0 border-b-0" />
 		</div>
 	)
 }
