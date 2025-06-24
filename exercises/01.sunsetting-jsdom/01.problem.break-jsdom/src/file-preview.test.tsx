74469a5293db42ef231e948a6c26c68cf4d0a6a6
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-24 15:24:15 -0400

    Refactor file preview test to render `FilePreview` component and add assertions for file name and contents visibility

--- exercises/01.sunsetting-jsdom/01.problem.break-jsdom/src/file-preview.test.tsx
+++ exercises/01.sunsetting-jsdom/01.problem.break-jsdom/src/file-preview.test.tsx
@@ -1,16 +1,10 @@
 import '@testing-library/jest-dom/vitest'
 import { render, screen } from '@testing-library/react'
+import { FilePreview } from './file-preview'
 
 test('displays the preview card', () => {
-	// 🐨 Render the `FilePreview` component.
-	// 💰 render(<MyComponent />)
-	// Provide a new `File` instance as the value of the `file` prop.
-	// 💰 new File(['hello world'], 'file.txt')
-	//
-	// 🐨 Write an assertion that expects the file name to be visible.
-	// 💰 screen.getByText(expectedText)
-	// 💰 expect(element).toBeVisible()
-	//
-	// 🐨 Similarly, write another assertion that expects
-	// the file contents to be visible for the user.
+	render(<FilePreview file={new File(['hello world'], 'file.txt')} />)
+
+	expect(screen.getByText('file.txt')).toBeVisible()
+	expect(screen.getByText('hello world')).toBeVisible()
 })
