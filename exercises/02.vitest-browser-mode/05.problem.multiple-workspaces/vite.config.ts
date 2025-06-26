99025664906fbdd52fa898960f820522444d0cad
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-25 21:32:17 -0400

    Refactor vite.config.ts to remove unnecessary comments and streamline the configuration for multiple workspaces, focusing on test properties for unit and browser tests.

--- exercises/02.vitest-browser-mode/05.problem.multiple-workspaces/vite.config.ts
+++ exercises/02.vitest-browser-mode/05.problem.multiple-workspaces/vite.config.ts
@@ -43,46 +43,4 @@ export default defineConfig({
 			},
 		],
 	},
-	//
-	// 🐨 Add the `include` property and set it to an array
-	// with the only entry "**/*.test.ts".
-	//
-	// 🐨 Since "**/*.test.ts" will match ALL tests, exclude
-	// the browser tests by providing the `exclude` property
-	// and including the browser test pattern there.
-	// 🦉 Vitest workspaces override the default configuration,
-	// which means that the `exclude` array must include the
-	// default values to ignore tests from `node_modules`, for example.
-	// 💰 exclude: [...defaultConfig.exclude, '**/*.browser.test.ts(x)?']
-	//
-	// Now, switch to the second entry in the `workspace` array.
-	// 🐨 First, set the `extends` property to `true`. Let's extend the
-	// root-level options, like `plugins` to have consistent behavior in prod and tests.
-	// 💰 { extends: true, test: {} }
-	//
-	// 🐨 Next, add these properties to the `test` in this workspace:
-	// {
-	//   name: "browser",
-	//   globals: true,
-	//   include: ["**/*.browser.test.ts(x)?"]
-	// }
-	//
-	// 🐨 Finally, copy the existing `browser` configuration
-	// under the `test` property of the second workspace.
-	// 💰 { test: { name: 'browser', browser: {...} }}
-	//
-	// 💣 Delete this root-level `test` property altogether.
-	// test: {
-	// 	globals: true,
-	// 	browser: {
-	// 		enabled: true,
-	// 		provider: 'playwright',
-	// 		instances: [
-	// 			{
-	// 				browser: 'chromium',
-	// 				setupFiles: ['./vitest.browser.setup.ts'],
-	// 			},
-	// 		],
-	// 	},
-	// },
 })
