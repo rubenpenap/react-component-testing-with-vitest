a64d0dc4013f5ae90205ee773b0c616a14e8f4dd
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-26 17:55:00 -0400

    Add handlers.ts file to define HTTP request handlers for discount code mocking using MSW.

---
+++ exercises/03.best-practices/03.problem.network-mocking/src/mocks/handlers.ts
@@ -0,0 +1,15 @@
+import { http, HttpResponse } from 'msw'
+import type { Discount } from '../discount-code-form'
+
+export const handlers = [
+	http.post<never, string, Discount>(
+		'https://api.example.com/discount/code',
+		async ({ request }) => {
+			const code = await request.text()
+			return HttpResponse.json({
+				code,
+				amount: 20,
+			})
+		},
+	),
+]
