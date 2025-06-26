df9a3ee623e3ed2d5f046d350688ecf88e9431e0
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-26 14:42:16 -0400

    Refactor discount code form test to remove comments and implement assertions for visibility of input and button elements.

--- exercises/03.best-practices/01.problem.queries/src/discount-code-form.browser.test.tsx
+++ exercises/03.best-practices/01.problem.queries/src/discount-code-form.browser.test.tsx
@@ -5,17 +5,10 @@ import { DiscountCodeForm } from './discount-code-form'
 test('renders the discount form', async () => {
 	render(<DiscountCodeForm />)
 
-	// 🐨 Create a variable called `discountInput` and assign it
-	// the result of locating the element by label text "Discount code".
-	// 💰 const discountInput = page.getByLabelText(labelText)
-	//
-	// 🐨 Write an assertion that the `discountInput` element is visible.
-	// 💰 await expect.element(locator).toBeVisible()
-	//
-	// 🐨 Create a new variable called `applyDiscountButton` and assign it
-	// the result of locating the element by role 'button' and accessible name
-	// "Apply discount".
-	// 💰 const applyDiscountButton = page.getByRole(role, { name: accessibleName })
-	//
-	// 🐨 Finally, write another assertion that the apply discount button is visible.
+	const discountInput = page.getByLabelText('Discount code')
+	await expect.element(discountInput).toBeVisible()
+	const applyDiscountButton = page.getByRole('button', {
+		name: 'Apply discount',
+	})
+	await expect.element(applyDiscountButton).toBeVisible()
 })
