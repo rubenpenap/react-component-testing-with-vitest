daa35a2a50754b197a41ce500bf317f1f0763775
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-27 12:38:31 -0400

    Replace commented breakpoints with actual debugger statements in TicTacToe browser tests to facilitate debugging during test execution.

--- exercises/04.debugging/02.problem.debugger/src/tic-tac-toe.browser.test.tsx
+++ exercises/04.debugging/02.problem.debugger/src/tic-tac-toe.browser.test.tsx
@@ -7,21 +7,15 @@ test('places cross marks in a horizontal line', async () => {
 
 	await page.getByRole('button', { name: 'left middle' }).click()
 
-	// 🐨 Add a breakpoint here to pause the test execution after
-	// it marks a cross in the game.
-	// 💰 debugger
+	debugger
 
 	await page.getByRole('button', { name: 'middle', exact: true }).click()
 
-	// 🐨 Add a breakpoint here to pause the test execution after
-	// it marks a cross in the game.
-	// 💰 debugger
+	debugger
 
 	await page.getByRole('button', { name: 'right middle' }).click()
 
-	// 🐨 Add a breakpoint here to pause the test execution after
-	// it marks a cross in the game.
-	// 💰 debugger
+	debugger
 
 	const squares = page.getByRole('button').elements().slice(3, 6)
 	expect(squares.map((element) => element.textContent)).toEqual(['✗', '✗', '✗'])
