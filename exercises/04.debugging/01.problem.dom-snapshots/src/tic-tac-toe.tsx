a72c050ea587d879c71cf3e07c5668d6e2e9f2de
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-06-27 12:27:16 -0400

    Fix button order in TicTacToe component by moving the "bottom left" button to the correct position, ensuring proper layout and functionality.

--- exercises/04.debugging/01.problem.dom-snapshots/src/tic-tac-toe.tsx
+++ exercises/04.debugging/01.problem.dom-snapshots/src/tic-tac-toe.tsx
@@ -8,8 +8,8 @@ export function TicTacToe() {
 			<Button aria-label="top right" className="border-t-0 border-r-0" />
 			<Button aria-label="left middle" className="border-l-0" />
 			<Button aria-label="middle" />
-			<Button aria-label="bottom left" className="border-r-0" />
 			<Button aria-label="right middle" className="border-b-0 border-l-0" />
+			<Button aria-label="bottom left" className="border-r-0" />
 			<Button aria-label="bottom middle" className="border-b-0" />
 			<Button aria-label="bottom right" className="border-r-0 border-b-0" />
 		</div>
